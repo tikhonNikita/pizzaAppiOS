33d3458bc16c9a5a8e3c9f3055ee172512891af4
Nikita Tikhonov <jagarnikita@gmail.com>
2023-11-10 19:03:33 +0200

    Initial Commit

---
+++ Pizza/PizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaApp.swift
+//  Pizza
+//
+//  Created by Nikita Tikhonov on 10.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct PizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
